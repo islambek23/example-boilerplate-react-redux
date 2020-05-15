048f3520ac1ab3cc5da78a3d59f70b46e426cceb
Islambek <islambek.dzhumaliev@gmail.com>
2020-05-16 01:51:28 +0600

    server requests updated

--- server/server.js
+++ server/server.js
@@ -4,9 +4,12 @@ import path from 'path'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import sockjs from 'sockjs'
+import axios from 'axios'
 
 import cookieParser from 'cookie-parser'
 import Html from '../client/html'
+
+const { readFile, writeFile, unlink } = require('fs').promises
 
 let connections = []
 
@@ -20,6 +23,61 @@ server.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit
 server.use(bodyParser.json({ limit: '50mb', extended: true }))
 
 server.use(cookieParser())
+
+const saveFile = async (users) => {
+  await writeFile(`${__dirname}/test.json`, JSON.stringify(users), { encoding: 'utf8' })
+}
+
+const readingFile = () => {
+  return readFile(`${__dirname}/test.json`, { encoding: 'utf8' })
+    .then((data) => JSON.parse(data))
+    .catch(async () => {
+      const { data: users } = await axios.get('https://jsonplaceholder.typicode.com/users')
+      await saveFile(users)
+      return users
+    })
+}
+
+server.get('/api/v1/users/', async (req, res) => {
+  const users = await readingFile()
+  res.json(users)
+})
+
+server.post('/api/v1/users/', async (req, res) => {
+  const newUser = req.body
+  const users = await readingFile()
+  const newId = users[users.length - 1].id + 1
+  const newUsers = [...users, { id: newId, ...newUser }]
+  await saveFile(newUsers)
+  res.json({ status: 'success', id: newId })
+})
+
+server.patch('/api/v1/users/:userId', async (req, res) => {
+  const updateUser = req.body
+  const { userId } = req.params
+  const users = await readingFile()
+  const updateUsers = users.reduce((acc, rec) => {
+    if (rec.id === +userId) {
+      return [...acc, { ...rec, ...updateUser }]
+    }
+    return [...acc, rec]
+  }, [])
+  await saveFile(updateUsers)
+  res.json({ status: 'success', id: userId })
+})
+
+server.delete('/api/v1/users/:userId', async (req, res) => {
+  const { userId } = req.params
+  const users = await readingFile()
+  const filteredUsers = users.filter((el) => el.id !== Number(userId))
+  await saveFile(filteredUsers)
+  res.json({ success: 'Success' })
+})
+
+server.delete('/api/v1/users/', (req, res) => {
+  unlink(`${__dirname}/test.json`)
+  res.json({ status: 'Success del all users' })
+})
 
 server.use('/api/', (req, res) => {
   res.status(404)
@@ -66,3 +124,8 @@ echo.installHandlers(app, { prefix: '/ws' })
 
 // eslint-disable-next-line no-console
 console.log(`Serving at http://localhost:${port}`)
+
+// server.get('/api/v1/users', async (req, res) => {
+//     const { data: users } = await readFile(`${__dirname}/test.json`, {encoding: 'utf8'})
+//     res.json(users)
+// })
